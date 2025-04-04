def45d7e5e5fdab99f85af40ee4b0433d0bed8fb
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-04-04 19:37:20 -0300

    ej1 foldDoc hecho

--- src/Documento.hs
+++ src/Documento.hs
@@ -10,7 +10,7 @@ module Documento
     imprimir,
   )
 where
-
+ 
 data Doc
   = Vacio
   | Texto String Doc
@@ -28,8 +28,14 @@ texto t | '\n' `elem` t = error "El texto no debe contener saltos de línea"
 texto [] = Vacio
 texto t = Texto t Vacio
 
+--foldr f z [] = z
+--foldr f z (x : xs) = f x (foldr f z xs)
+
 -- foldDoc :: ... PENDIENTE: Ejercicio 1 ...
-foldDoc = error "PENDIENTE: Ejercicio 1"
+foldDoc :: b -> (String -> b -> b) -> (Int -> b -> b) -> Doc -> b
+foldDoc base fTexto fLinea (Vacio) = base
+foldDoc base fTexto fLinea (Texto str doc) = fTexto str (foldDoc base fTexto fLinea doc)
+foldDoc base fTexto fLinea (Linea n doc) = fLinea n (foldDoc base fTexto fLinea doc)
 
 -- NOTA: Se declara `infixr 6 <+>` para que `d1 <+> d2 <+> d3` sea equivalente a `d1 <+> (d2 <+> d3)`
 -- También permite que expresiones como `texto "a" <+> linea <+> texto "c"` sean válidas sin la necesidad de usar paréntesis.
