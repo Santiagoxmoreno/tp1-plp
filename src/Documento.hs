bcdbd2e499d42b6d2955482759ab5465525a7f6a
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-04-10 14:44:23 -0300

    Pequeño refactor

--- src/Documento.hs
+++ src/Documento.hs
@@ -28,10 +28,6 @@ texto t | '\n' `elem` t = error "El texto no debe contener saltos de línea"
 texto [] = Vacio
 texto t = Texto t Vacio
 
---foldr f z [] = z
---foldr f z (x : xs) = f x (foldr f z xs)
-
--- foldDoc :: ... PENDIENTE: Ejercicio 1 ...
 foldDoc :: b -> (String -> b -> b) -> (Int -> b -> b) -> Doc -> b
 foldDoc base fTexto fLinea (Vacio) = base
 foldDoc base fTexto fLinea (Texto str doc) = fTexto str (foldDoc base fTexto fLinea doc)
@@ -43,21 +39,18 @@ infixr 6 <+>
 
 (<+>) :: Doc -> Doc -> Doc
 d1 <+> d2 = foldDoc d2 fTexto fLinea d1
-            where fTexto = (\text documento -> if esTexto(documento) then concatenarTextos text documento else (Texto text documento)) -- si son textos se concatenan, y si es linea o vacio se ponen al final
-                  fLinea = (\n documento -> if esTexto(documento) then Linea n (documento) else lineaConLinea n documento)
+            where fTexto txt doc = if esTexto(doc) then concatenarTextos txt doc else (Texto txt doc) -- si son textos se concatenan, y si es linea o vacio se ponen al final
+                  fLinea n doc = if esTexto(doc) then Linea n (doc) else lineaConLinea n doc
                   concatenarTextos str (Texto str2 doc) = Texto (str ++ str2) doc --la concatenacion no puede producir string vacio, ni salto de linea, si ambos cumplen el invariante
                   lineaConLinea n1 (Linea n2 doc2) = Linea n1 (Linea n2 doc2) --no se modifican las i de las lineas que estamos concatenando, entonces si no son menores a 0 el resultado tampoco
                   esTexto (Texto _ _) = True
                   esTexto _ = False
 
 indentar :: Int -> Doc -> Doc
-indentar i = foldDoc Vacio (\x rec -> Texto x rec) (\x rec -> Linea (x + i) rec) --si la linea no es negativa, entonces indentar no la va a hacer negativa, porque le suma un int mayor a 0
+indentar i = foldDoc Vacio (Texto) (\x rec -> Linea (x + i) rec) --no modifica al texto, y si la linea no es negativa, entonces indentar no la va a hacer negativa, porque le suma un int mayor a 0
 
 mostrar :: Doc -> String
 mostrar = foldDoc "" (++) (\x rec -> "\n" ++ (foldr (++) [] $ take x $ repeat " ") ++ rec)
-
---[1,2,3]
---f 1 (f 2 (f 3 base))
 
 -- | Función dada que imprime un documento en pantalla
 
