542b069c5c24f79bc5079f8f85041296bd61940b
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-05-22 19:03:17 -0300

    final

--- src/Documento.hs
+++ src/Documento.hs
@@ -38,13 +38,13 @@ foldDoc base fTexto fLinea (Linea n doc) = fLinea n (foldDoc base fTexto fLinea 
 infixr 6 <+>
 
 (<+>) :: Doc -> Doc -> Doc
-d1 <+> d2 = foldDoc d2 fTexto Linea d1 -- si d1 es una linea, se le agrega d2 al final
-            where fTexto txt (Texto str doc) = Texto (txt ++ str) doc -- si son dos textos se concatenan en uno, asi se mantiene el invariante
-                  fTexto txt doc = Texto txt doc  -- si no son dos textos, simplemente se juntan
+d1 <+> d2 = foldDoc d2 fTexto Linea d1 -- si d1 es una linea, se le agrega d2 al final, y mantiene el i >= 0 que ya tenia
+            where fTexto str1 (Texto str2 doc) = Texto (str1 ++ str2) doc -- si son dos textos se concatenan en uno, asi se mantiene el invariante, y como suponemos que "(Texto str2 doc)" cuple el invariante, doc es una linea o vacio, entonces "Texto (str1 ++ str2) doc" también cumple
+                  fTexto str doc = Texto str doc  -- si no son dos textos, simplemente se juntan. Como "str doc" cumple el invariante, doc es linea o vacio, por lo que "Texto str doc" también cumple 
 
 indentar :: Int -> Doc -> Doc
 indentar i = foldDoc Vacio (Texto) (\x rec -> Linea (x + i) rec) --no modifica al texto, y si la linea no es negativa, entonces indentar no la va a hacer negativa, porque le suma un int mayor a 0
-
+ 
 mostrar :: Doc -> String
 mostrar = foldDoc "" (++) (\x rec -> "\n" ++ (replicate x ' ') ++ rec)
 
